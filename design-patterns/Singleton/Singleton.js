4a296b8a111a25402a7124d2f0918f15e19f9b6c
laplandin <deniskakhv@gmail.com>
2019-05-11 15:29:38 +0300

    singleton comments and IIFE variant

--- design-patterns/Singleton/Singleton.js
+++ design-patterns/Singleton/Singleton.js
@@ -1,3 +1,9 @@
+/* Simple object can be Singleton, because we work with single instance of it.
+*  We can define instance on demand, but in this realization
+*  we can't to pass new params every time we want to re-instantiate it.
+*  Instead we mutate the instance in place,
+*  or we can write some method, which can do it for us
+*  */
 const ClassLessSingletonLiteral = {
 	get instance() {
 		if (!this._instance) {
@@ -18,10 +24,15 @@ const ClassLessSingletonLiteral = {
 	},
 };
 
-
+/* Similar approach, which make new instance like a module before export
+*  it to the outer world.
+*  We can't call it after through "new" keyword.
+*  Also the same disadvantages, as: if constructor start before export,
+*  we can't change it state after.
+* */
 class ExportedSingletonInstance {
-	constructor() {
-		this.prop = 'prop';
+	constructor(prop) {
+		this.prop = prop;
 	}
 	
 	static staticMethod() {
@@ -34,11 +45,12 @@ class ExportedSingletonInstance {
 }
 
 class ClassPropSingleton {
-	constructor() {
+	constructor(prop) {
 		if (ClassPropSingleton.instance && typeof ClassPropSingleton.instance === 'object') {
+			ClassPropSingleton._applyParams(prop);
 			return ClassPropSingleton.instance;
 		}
-		this.prop = 'prop';
+		this.prop = prop;
 		ClassPropSingleton.instance = this;
 	}
 	
@@ -49,11 +61,36 @@ class ClassPropSingleton {
 	method() {
 		return this.prop;
 	}
+	
+	static _applyParams(prop) {
+		ClassPropSingleton.instance.prop = prop;
+	}
 }
+
+const IIFESingleton = {
+	get instance() {
+		return this.getInstance();
+	},
+	getInstance: (function(){
+		let instance;
+		return function() {
+			if (!instance) {
+				instance = {
+					prop: 'test1',
+					method() {
+						return this.prop;
+					}
+				}
+			}
+			return instance;
+		}
+	}())
+};
 
 
 module.exports = {
 	ExportedSingletonInstance: new ExportedSingletonInstance(),
 	ClassLessSingletonLiteral,
 	ClassPropSingleton,
+	IIFESingleton,
 };
--- design-patterns/Singleton/Singleton.test.js
+++ design-patterns/Singleton/Singleton.test.js
@@ -2,13 +2,14 @@ const Singleton = require('./Singleton');
 
 function iExpect(first, second) {
 	expect(first).toBe(second);
-	expect(first.method()).toBe('prop');
+	expect(first.method()).toBe('test2');
 }
 
 test('Test classless singleton object', () => {
 	const { ClassLessSingletonLiteral } = Singleton;
 	const first = ClassLessSingletonLiteral.instance;
 	const second = ClassLessSingletonLiteral.instance;
+	second._instance.prop = 'test2';
 	
 	iExpect(first, second);
 	expect(first.staticMethod()).toBe(42);
@@ -19,6 +20,7 @@ test('Test for Exported Singleton Instance', () => {
 	// We cannot call this instance with 'new', because of it already exported instantiated
 	const first = ExportedSingletonInstance;
 	const second = ExportedSingletonInstance;
+	second.prop = 'test2';
 	
 	iExpect(first, second);
 	expect(first.constructor.staticMethod()).toBe(42);
@@ -27,9 +29,17 @@ test('Test for Exported Singleton Instance', () => {
 test('Test for class own prop singleton', () => {
 	const { ClassPropSingleton } = Singleton;
 	
-	const first = new ClassPropSingleton();
-	const second = new ClassPropSingleton();
+	const first = new ClassPropSingleton('test1');
+	const second = new ClassPropSingleton('test2');
 	
 	iExpect(first, second);
 	expect(ClassPropSingleton.staticMethod()).toBe(42);
+});
+
+test('Test for IIFE Singleton', () => {
+	const { IIFESingleton } = Singleton;
+	const first = IIFESingleton.instance;
+	const second = IIFESingleton.instance;
+	second.prop = 'test2';
+	iExpect(first, second);
 });
